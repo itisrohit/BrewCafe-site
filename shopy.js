b163c7f65bab9bd762c19e2abe80870f07627824
Rohit Kumar <dv6510@gmail.com>
2024-10-26 21:39:40 +0530

    done

--- shopy.js
+++ shopy.js
@@ -10,13 +10,17 @@ document.addEventListener("DOMContentLoaded", () => {
         contentDiv.innerHTML = ''; 
 
         data.data.forEach(item => {
+          const isInCart = checkIfInCart(item.id);
+
           const coffeeItemHTML = `
             <div class="coffee-item">
               <a href="#" style="text-decoration: none; color: inherit;">
                 <img src="${item.image}" alt="${item.title}">
                 <h3>${item.title}</h3>
                 <p>Price: ₹${item.price}</p>
-                <button class="add-to-cart" data-id="${item.id}" data-title="${item.title}" data-price="${item.price}" data-image="${item.image}">Add to Cart</button>
+                <button class="add-to-cart" data-id="${item.id}" data-title="${item.title}" data-price="${item.price}" data-image="${item.image}">
+                  ${isInCart ? 'Added' : 'Add to Cart'}
+                </button>
               </a>
             </div>
           `;
@@ -34,7 +38,12 @@ document.addEventListener("DOMContentLoaded", () => {
               price: addToCartButton.getAttribute('data-price'),
               image: addToCartButton.getAttribute('data-image')
             };
-            addToCart(item);
+            if (!checkIfInCart(item.id)) {
+              addToCart(item);
+              addToCartButton.textContent = 'Added';
+            } else {
+              alert('Item is already in the cart.');
+            }
           });
         });
       } else {
@@ -47,15 +56,15 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-// Function to add item to localStorage
+
+function checkIfInCart(itemId) {
+  const cart = JSON.parse(localStorage.getItem('cart')) || [];
+  return cart.some(cartItem => cartItem.id === itemId);
+}
+
+
 function addToCart(item) {
   let cart = JSON.parse(localStorage.getItem('cart')) || [];
-  const itemExists = cart.some(cartItem => cartItem.id === item.id);
-
-  if (itemExists) {
-    alert('Item is already in the cart.');
-  } else {
-    cart.push(item);
-    localStorage.setItem('cart', JSON.stringify(cart));
-  }
+  cart.push(item);
+  localStorage.setItem('cart', JSON.stringify(cart));
 }
